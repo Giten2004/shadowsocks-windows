aefb7e68f8b2b84f389fa8825e56ddf15378b977
Michael Xu <xubo2004@gmail.com>
2016-03-22 00:29:02 +0800

    http://www.ishadowsocks.net/

---
+++ shadowsocks-csharp/Controller/Service/FreeSSServerUpdater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shadowsocks.Controller
+{
+    public class FreeSSServerUpdater
+    {
+        private const string IShadowsocks_URL = "http://www.ishadowsocks.net/";
+    }
+}
